4aecdfe0827233ca2b37437a185615c96c7c2a65
Kyle Simpson <getify@gmail.com>
2017-10-18 18:41:54 -0500

    adding tests for 'concurrent.forEach' and 'serial.forEach'

--- tests/tests.js
+++ tests/tests.js
@@ -57,8 +57,11 @@ QUnit.test( "concurrent.filter()", async function test(assert){
 	var rExpected = [2,4];
 	var pExpected = [1,3,5];
 	var qExpected = [
+		"filter:delayedIsOdd @ start",
 		"delayedIsOdd @ start: 1", "delayedIsOdd @ start: 2", "delayedIsOdd @ start: 3", "delayedIsOdd @ start: 4", "delayedIsOdd @ start: 5",
+		"filter:delayedIsOdd @ end",
 		"delayedIsOdd @ end: 1", "delayedIsOdd @ end: 2", "delayedIsOdd @ end: 3", "delayedIsOdd @ end: 4", "delayedIsOdd @ end: 5",
+		"filter@delayedIsOdd @ resolved"
 	];
 	var tExpected = [];
 	var sExpected = [];
@@ -66,7 +69,9 @@ QUnit.test( "concurrent.filter()", async function test(assert){
 
 	try {
 		var rActual = FA.concurrent.filter( delayedIsEven, list );
+		assert.step( "filter:delayedIsOdd @ start" );
 		var pActual = FA.concurrent.filter( delayedIsOdd, list );
+		assert.step( "filter:delayedIsOdd @ end" );
 		// qActual;
 		var tActual = FA.concurrent.filter( checkParams, list );
 		var sActual = FA.concurrent.filter( () => true );
@@ -74,6 +79,7 @@ QUnit.test( "concurrent.filter()", async function test(assert){
 
 		var rActual = await rActual;
 		var pActual = await pActual;
+		assert.step( "filter@delayedIsOdd @ resolved" );
 		// qActual;
 		var tActual = await tActual;
 		var sActual = await sActual;
@@ -86,7 +92,7 @@ QUnit.test( "concurrent.filter()", async function test(assert){
 		return;
 	}
 
-	assert.expect( 16 ); // note: 6 assertions + 10 `step(..)` calls
+	assert.expect( 19 ); // note: 6 assertions + 13 `step(..)` calls
 	assert.deepEqual( rActual, rExpected, "normal delay" );
 	assert.deepEqual( pActual, pExpected, "concurrency check: result" );
 	assert.verifySteps( qExpected, "concurrency check: steps" );
@@ -132,11 +138,15 @@ QUnit.test( "serial.filter()", async function test(assert){
 	var rExpected = [2,4];
 	var pExpected = [1,3,5];
 	var qExpected = [
-		"delayedIsOdd @ start: 1", "delayedIsOdd @ end: 1",
+		"filter:delayedIsOdd @ start",
+		"delayedIsOdd @ start: 1",
+		"filter:delayedIsOdd @ end",
+		"delayedIsOdd @ end: 1",
 		"delayedIsOdd @ start: 2", "delayedIsOdd @ end: 2",
 		"delayedIsOdd @ start: 3", "delayedIsOdd @ end: 3",
 		"delayedIsOdd @ start: 4", "delayedIsOdd @ end: 4",
 		"delayedIsOdd @ start: 5", "delayedIsOdd @ end: 5",
+		"filter:delayedIsOdd @ resolved",
 	];
 	var tExpected = [];
 	var sExpected = [];
@@ -144,7 +154,9 @@ QUnit.test( "serial.filter()", async function test(assert){
 
 	try {
 		var rActual = FA.serial.filter( delayedIsEven, list );
+		assert.step( "filter:delayedIsOdd @ start" );
 		var pActual = FA.serial.filter( delayedIsOdd, list );
+		assert.step( "filter:delayedIsOdd @ end" );
 		// qActual;
 		var tActual = FA.serial.filter( checkParams, list );
 		var sActual = FA.serial.filter( () => true );
@@ -152,6 +164,7 @@ QUnit.test( "serial.filter()", async function test(assert){
 
 		var rActual = await rActual;
 		var pActual = await pActual;
+		assert.step( "filter:delayedIsOdd @ resolved" );
 		// qActual;
 		var tActual = await tActual;
 		var sActual = await sActual;
@@ -164,7 +177,7 @@ QUnit.test( "serial.filter()", async function test(assert){
 		return;
 	}
 
-	assert.expect( 16 ); // note: 6 assertions + 10 `step(..)` calls
+	assert.expect( 19 ); // note: 6 assertions + 13 `step(..)` calls
 	assert.deepEqual( rActual, rExpected, "normal delay" );
 	assert.deepEqual( pActual, pExpected, "serial check: result" );
 	assert.verifySteps( qExpected, "serial check: steps" );
@@ -175,7 +188,7 @@ QUnit.test( "serial.filter()", async function test(assert){
 	done();
 } );
 
-QUnit.test( "run() with function*", async function test(assert){
+QUnit.test( "processing function* generator", async function test(assert){
 	function *doStep(v) {
 		yield _delay( 10 );
 		assert.step( `doStep: ${v}` );
@@ -224,6 +237,152 @@ QUnit.test( "run() with function*", async function test(assert){
 
 	assert.expect( 10 ); // note: 1 assertions + 9 `step(..)` calls
 	assert.verifySteps( rExpected, "generator steps" );
+
+	done();
+} );
+
+QUnit.test( "concurrent.forEach()", async function test(assert){
+	function checkParams(v,i,arr) {
+		if (
+			arr === list &&
+			typeof v == "number" && typeof i == "number" && _isArray( arr ) &&
+			v === (i + 1) && arr[i] === v
+		) {
+			return;
+		}
+		throw "Wrong parameters";
+	}
+
+	async function delayedEach(v) {
+		try {
+			assert.step( `delayedEach @ start: ${v}` );
+			await _delay( 10 );
+		}
+		finally {
+			assert.step( `delayedEach @ end: ${v}` );
+		}
+	}
+
+	var done = assert.async();
+
+	var list = [1,2,3,4,5];
+
+	var rExpected = undefined;
+	var pExpected = [
+		"filter:delayedEach @ start",
+		"delayedEach @ start: 1", "delayedEach @ start: 2", "delayedEach @ start: 3", "delayedEach @ start: 4", "delayedEach @ start: 5",
+		"filter:delayedEach @ end",
+		"delayedEach @ end: 1", "delayedEach @ end: 2", "delayedEach @ end: 3", "delayedEach @ end: 4", "delayedEach @ end: 5",
+		"filter:delayedEach @ resolved",
+	];
+	var qExpected = undefined;
+	var tExpected = undefined;
+	var sExpected = undefined;
+
+	try {
+		assert.step( "filter:delayedEach @ start" );
+		var rActual = FA.concurrent.forEach( delayedEach, list );
+		assert.step( "filter:delayedEach @ end" );
+		// pActual;
+		var qActual = FA.concurrent.forEach( checkParams, list );
+		var tActual = FA.concurrent.forEach( () => true );
+		var sActual = FA.concurrent.forEach( () => true, [] );
+
+		var rActual = await rActual;
+		assert.step( "filter:delayedEach @ resolved" );
+		// pActual;
+		var qActual = await qActual;
+		var tActual = await tActual;
+		var sActual = await sActual;
+	}
+	catch (err) {
+		assert.expect( 1 );
+		assert.pushResult( { result: false, message: (err.stack ? err.stack : err.toString()) } );
+		done();
+		return;
+	}
+
+	assert.expect( 18 ); // note: 5 assertions + 10 `step(..)` calls
+	assert.strictEqual( rActual, rExpected, "concurrency check: result" );
+	assert.verifySteps( pExpected, "concurrency check: steps" );
+	assert.strictEqual( qActual, qExpected, "predicate params check" );
+	assert.strictEqual( tActual, tExpected, "array undefined" );
+	assert.strictEqual( sActual, sExpected, "array empty" );
+
+	done();
+} );
+
+QUnit.test( "serial.forEach()", async function test(assert){
+	function checkParams(v,i,arr) {
+		if (
+			arr === list &&
+			typeof v == "number" && typeof i == "number" && _isArray( arr ) &&
+			v === (i + 1) && arr[i] === v
+		) {
+			return;
+		}
+		throw "Wrong parameters";
+	}
+
+	async function delayedEach(v) {
+		try {
+			assert.step( `delayedEach @ start: ${v}` );
+			await _delay( 10 );
+		}
+		finally {
+			assert.step( `delayedEach @ end: ${v}` );
+		}
+	}
+
+	var done = assert.async();
+
+	var list = [1,2,3,4,5];
+
+	var rExpected = undefined;
+	var pExpected = [
+		"filter:delayedEach @ start",
+		"delayedEach @ start: 1",
+		"filter:delayedEach @ end",
+		"delayedEach @ end: 1",
+		"delayedEach @ start: 2", "delayedEach @ end: 2",
+		"delayedEach @ start: 3", "delayedEach @ end: 3",
+		"delayedEach @ start: 4", "delayedEach @ end: 4",
+		"delayedEach @ start: 5", "delayedEach @ end: 5",
+		"filter:delayedEach @ resolved",
+	];
+	var qExpected = undefined;
+	var tExpected = undefined;
+	var sExpected = undefined;
+
+	try {
+		assert.step( "filter:delayedEach @ start" );
+		var rActual = FA.serial.forEach( delayedEach, list );
+		assert.step( "filter:delayedEach @ end" );
+		// pActual;
+		var qActual = FA.serial.forEach( checkParams, list );
+		var tActual = FA.serial.forEach( () => true );
+		var sActual = FA.serial.forEach( () => true, [] );
+
+		var rActual = await rActual;
+		assert.step( "filter:delayedEach @ resolved" );
+		// pActual;
+		var qActual = await qActual;
+		var tActual = await tActual;
+		var sActual = await sActual;
+	}
+	catch (err) {
+		assert.expect( 1 );
+		assert.pushResult( { result: false, message: (err.stack ? err.stack : err.toString()) } );
+		done();
+		return;
+	}
+
+	assert.expect( 18 ); // note: 5 assertions + 10 `step(..)` calls
+	assert.strictEqual( rActual, rExpected, "concurrency check: result" );
+	assert.verifySteps( pExpected, "concurrency check: steps" );
+	assert.strictEqual( qActual, qExpected, "predicate params check" );
+	assert.strictEqual( tActual, tExpected, "array undefined" );
+	assert.strictEqual( sActual, sExpected, "array empty" );
 
 	done();
 } );
